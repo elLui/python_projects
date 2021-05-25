ac1b6ba155f473f27f139ac227bd890ec4ab09b6
Luis Cortez <elluis.codes@gmail.com>
2021-05-25 14:04:24 -0500

    commented out print of all projects on profile
    TODO :: use get request to load project data for job_finder

--- todoist_job_followUps.py
+++ todoist_job_followUps.py
@@ -1,28 +1,39 @@
 #! python3
 
 # todoist scheduling application :: https://todoist-python.readthedocs.io/en/latest/modules.html
+import pprint
+import requests
 import todoist
 
-
-
-import datetime
+# imports to handle grabbing api key from .gitignore file
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
+import datetime
+
+
+def update():
+    api.sync()
 
 
 if __name__ == '__main__':
 
-
+    # grab api_key from environment variable
     get_api = os.getenv('api_key')
+    # transform api with todoist library
     api = todoist.TodoistAPI(get_api)
-    api.sync()
+    # get current todoist data
 
-
-    print(api.state['projects'])
-
-
-
+    # to test functionality - will grab all projects and print to console
+    ##########################################################################################################
+    # # console log raw current projects
+    # pprint.pprint(api.state['projects'])
+    #
+    # # for project in api.state['projects']:
+    # #     update()
+    # #     print(project['name'])
 
 
 
