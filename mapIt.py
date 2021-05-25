5742b04afae26d5a1a7cadd69ccf5ebe200e0339
Luis Cortez <elluis.codes@gmail.com>
2021-05-25 13:12:20 -0500

    fixed webbrowser not opening a new browser instance by adding .open_new :: webbrowser.open_new fixed address not parsing spaces by adding ' '.join

--- mapIt.py
+++ mapIt.py
@@ -1,6 +1,7 @@
 #! python3
 
 
+# prior to using must install pyperclip :: pip3 install pyperclip
 # mapIt.py - use google maps to find an address either from the cli or clipboard (pyperclip)
 
 import webbrowser, sys, pyperclip
@@ -8,15 +9,15 @@ import webbrowser, sys, pyperclip
 
 # look for an address on the cli 
 if len(sys.argv) > 1:
-    # join address as an array of strings
-    address = ''.join(sys.argv[1:])
+    # join address as an array of strings :: ' ' denotes a space between each argument
+    address = ' '.join(sys.argv[1:])
 
     # if no arguments are passed check clipboard for contents
 else:
     address = pyperclip.paste()
 
 # open default webbrowser with contents of address
-webbrowser.open('https://www.google.com/maps/place/' + address)
+webbrowser.open_new('https://www.google.com/maps/place/' + address)
 
 
 
