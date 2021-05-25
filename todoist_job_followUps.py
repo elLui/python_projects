d58e7eeefd5f976852706e34bfab837a5f2e3c45
Luis Cortez <elluis.codes@gmail.com>
2021-05-25 11:30:29 -0500

    removed config.py because it was a stupid way to import a variable api key
    added import os
    added from dotenv import load_dotenv && load_env() // to reload .env variables
    
    added .env && *.env to .gitignore !!
    NEVER ADD .env to git
    
    run successful - retrieved account data after api.sync() && printed to console

--- todoist_job_followUps.py
+++ todoist_job_followUps.py
@@ -3,13 +3,23 @@
 # todoist scheduling application :: https://todoist-python.readthedocs.io/en/latest/modules.html
 import todoist
 
-# config.api_key location :: added to .gitignore
-import config
+
 
 import datetime
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
+if __name__ == '__main__':
 
+
+    get_api = os.getenv('api_key')
+    api = todoist.TodoistAPI(get_api)
+    api.sync()
+
+
+    print(api.state['projects'])
 
 
 
